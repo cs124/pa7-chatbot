bc1f554d296779a8cea740ef5ec6e42a854c309d
ehyuen <ethanyuen99@gmail.com>
2025-03-05 17:45:45 -0800

    part one complete

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ from pydantic import BaseModel, Field
 
 import numpy as np
 import re
-
+import random
 
 # noinspection PyMethodMayBeStatic
 class Chatbot:
@@ -36,6 +36,9 @@ class Chatbot:
 
         # Binarize the movie ratings before storing the binarized matrix.
         self.ratings = self.binarize(ratings)
+        self.user_ratings = np.zeros(np.shape(self.ratings)[0])
+        self.recommendations = []
+        self.awaiting_recommendation = False
         ########################################################################
         #                             END OF YOUR CODE                         #
         ########################################################################
@@ -50,7 +53,7 @@ class Chatbot:
         # TODO: Write a short greeting message                                 #
         ########################################################################
 
-        greeting_message = "How can I help you?"
+        greeting_message = "Hi! I'm a movie recommender bot. Tell me about a movie you've seen to get started!"
 
         ########################################################################
         #                             END OF YOUR CODE                         #
@@ -65,7 +68,7 @@ class Chatbot:
         # TODO: Write a short farewell message                                 #
         ########################################################################
 
-        goodbye_message = "Have a nice day!"
+        goodbye_message = "Hope I was helpful. See ya!"
 
         ########################################################################
         #                          END OF YOUR CODE                            #
@@ -105,13 +108,159 @@ class Chatbot:
         ########################################################################
         if self.llm_enabled:
             response = "I processed {} in LLM Programming mode!!".format(line)
+            return response
         else:
-            response = "I processed {} in Starter (GUS) mode!!".format(line)
+            affirmative_responses = [
+                "yes", "yeah", "yep", "ya", "yup", "sure", "of course", "okay", "ok", 
+                "alright", "absolutely", "certainly", "indeed", "affirmative", "right", 
+                "for sure", "you bet", "totally", "sure thing", "aye", "roger that", "y", "yes please"
+            ]
+            negative_responses = ["no", "n", "nah", "nope", "not really", "no thanks", "no thank you"]
+            
+            unsure_responses = [
+                "Hmm, I didn't quite catch that. Can you tell me about a movie you've seen recently?",
+                "I'm not sure I recognized a movie in what you said. Could you mention a title?",
+                "I couldn't identify a movie in your message. Let me know about one you've watched!"
+            ]
+            
+            multiple_titles_responses = [
+                "It looks like you mentioned multiple movies. Could you tell me about just one at a time?",
+                "I see more than one movie in your response! Let's focus on one for now.",
+                "You're talking about multiple movies! Pick one, and we can chat about it."
+            ]
+            
+            unknown_movie_responses = [
+                "Sorry, I couldn't find \"{}\" in my database. Could you try another movie?",
+                "\"{}\" doesn't seem to be in my records. Maybe try another one?",
+                "I couldn't locate \"{}\" in my database. Do you have another movie in mind?"
+            ]
+            
+            multiple_matches_responses = [
+                "I found multiple movies called \"{}\". Could you specify the year in parentheses along with the movie title?",
+                "There are several movies named \"{}\". Please include the release year in parentheses in your response.",
+                "\"{}\" has multiple versions! Try adding the year in parentheses to clarify which one you mean."
+            ]
+            
+            neutral_sentiment_responses = [
+                "I'm not sure how you feel about \"{}\". Could you clarify?",
+                "Hmm, I can't tell if you liked \"{}\" or not. Could you let me know?",
+                "It seems like you have neutral feelings about \"{}\". Care to elaborate?"
+            ]
+            
+            positive_sentiment_responses = [
+                "Great, so you liked \"{}\"! ",
+                "Awesome, you enjoyed \"{}\"! ",
+                "Nice! I see that you liked \"{}\". "
+            ]
+            
+            negative_sentiment_responses = [
+                "I see, you didn't enjoy \"{}\". ",
+                "Got it, you didn't like \"{}\". ",
+                "Understood, \"{}\" wasn't for you. "
+            ]
+
+            more_reviews_responses = [
+                "Tell me about more movies so I can make recommendations.",
+                "What other movies have you seen?",
+                "Let me know about more movies you've watched!"
+            ]
+
+            recommendation_ready_responses = [
+                "That's enough for me to make a recommendation!",
+                "Great! I have enough info to suggest a movie for you.",
+                "Awesome! I can now recommend something you might like."
+            ]
+
+            want_first_recommendation_responses = [
+                "Would you like me to recommend you a movie? (yes/no)", 
+                "Should I proceed with a recommendation? (yes/no)",
+                "Are you up for a recommendation? (yes/no)"
+            ]
+            
+            additional_recommendation_responses = [
+                "I think you might enjoy \"{}\". ",
+                "Based on your taste, you might like \"{}\"! ",
+                "\"{}\" could be a great match for you! "
+            ]
+
+            want_another_recommendation_responses = [
+                "Want another recommendation? (yes/no)", 
+                "Would you like more recommendations? (yes/no)",
+                "Interested in another recommendation? (yes/no)"
+            ]
+            
+            no_more_recommendations_responses = [
+                "I don't have any more recommendations for now! Let me know if you want to discuss more movies.",
+                "That's all the recommendations I have at the moment! Feel free to tell me about more movies.",
+                "No more suggestions from me for now! Let’s chat about more movies if you’d like."
+            ]
+            
+            ending_recommendation_responses = [
+                "Alright! Let me know if you want to talk about more movies.",
+                "Got it! If you ever want more recommendations, just ask.",
+                "No worries! I’m here whenever you want to discuss movies."
+            ]
+            
+            if self.llm_enabled:
+                return "I processed {} in LLM Programming mode!!".format(line)
+
+            if self.awaiting_recommendation:
+                line = re.sub(r"[^\w\s]", "", line)
+                if line.lower() in affirmative_responses:
+                    next_movie = self.recommendations.pop(0)
+                    if not self.recommendations:
+                        self.awaiting_recommendation = False
+                    return random.choice(additional_recommendation_responses).format(self.titles[next_movie][0]) + '\n' + (random.choice(want_another_recommendation_responses) if self.recommendations else random.choice(no_more_recommendations_responses))
+                elif line.lower() in negative_responses:
+                    self.awaiting_recommendation = False
+                    return random.choice(ending_recommendation_responses)
+                else:
+                    return "Please respond with 'yes' if you want another recommendation, or 'no' to stop."
+
+            titles = self.extract_titles(line)
+            if len(titles) == 0:
+                return random.choice(unsure_responses)
+
+            if len(titles) > 1:
+                return random.choice(multiple_titles_responses)
+
+            title = titles[0]
+            movie_indices = self.find_movies_by_title(title)
+            
+            if not movie_indices:
+                return random.choice(unknown_movie_responses).format(title)
+
+            if len(movie_indices) > 1:
+                return random.choice(multiple_matches_responses).format(title)
+
+            sentiment = self.extract_sentiment(line)
+            
+            if sentiment == 0:
+                return random.choice(neutral_sentiment_responses).format(title)
+            
+            idx = movie_indices[0]
+            self.user_ratings[idx] = sentiment
+            rated_movies = np.count_nonzero(self.user_ratings)
+
+            if rated_movies >= 5:
+                self.recommendations = self.recommend(self.user_ratings, self.ratings, k=5)
+                if self.recommendations:
+                    self.awaiting_recommendation = True
+                    sentiment_response = random.choice(positive_sentiment_responses).format(title) if sentiment > 0 else random.choice(negative_sentiment_responses).format(title)
+                    recommendation_ready_response = random.choice(recommendation_ready_responses)
+                    return sentiment_response + '\n' + recommendation_ready_response + '\n' + random.choice(want_first_recommendation_responses)
+
+            if sentiment > 0:
+                return random.choice(positive_sentiment_responses).format(title) + '\n' + random.choice(more_reviews_responses)
+
+            return random.choice(negative_sentiment_responses).format(title) + '\n' + random.choice(more_reviews_responses)
+
+
 
         ########################################################################
         #                          END OF YOUR CODE                            #
         ########################################################################
-        return response
+        # return response
 
     @staticmethod
     def preprocess(text):
@@ -191,7 +340,6 @@ class Chatbot:
         year_extract = re.search(r'\((\d{4})\)$', title)
         if year_extract:
             year_match = year_extract.group(1)
-            # Remove the year from the title for matching
             title = title.rsplit('(', 1)[0].strip()
 
         articles = ['The ', 'A ', 'An ']
@@ -207,23 +355,20 @@ class Chatbot:
 
         title_variations = [
             original_title,
-            title,  # Stripped title 
+            title,
             (title + real_article).strip()
         ]
-        print(year_match, title)
         for i, movie in enumerate(self.titles):
             movie_title = movie[0][:-7]
             movie_year = movie[0][-5:-1]
             
-            # print(movie_year[-6:])
-            # Check each title variation
+
             for variation in title_variations:
-                # If year is specified, match both title and year
                 if year_match:
                     if variation == movie_title and year_match == movie_year:
                         matching_indices.append(i)
                         break
-                # If no year specified, match just the title
+
                 else:
                     if variation == movie_title:
                         matching_indices.append(i)
@@ -248,41 +393,55 @@ class Chatbot:
         :returns: a numerical value for the sentiment of the text
         """
         stemmer = PorterStemmer()
+        stemmed_dict = {stemmer.stem(key): value for key, value in self.sentiment.items()}
         words = preprocessed_input.lower().split()  # Tokenize and normalize
         negation_words = {'not', 'no', 'never', 'neither', 'nor', "didn't", "wouldn't"}
+        emphasis_modifiers = {
+            'very': 2,
+            'extremely': 3,
+            'incredibly': 3,
+            'absolutely': 3,
+            'totally': 2,
+            'quite': 1.5,
+            'somewhat': 0.5,
+            'slightly': 0.5,
+            'barely': 0.25,
+            'kind of': 0.5,
+            'sort of': 0.5
+        }
     
-        # Sentiment calculation
+
         sentiment_score = 0
         negation_active = False
         in_quotes = False
-        
-        # Iterate through preprocessed words
+        emphasis_multiplier = 1.0
+
         for word in words:
             if word.startswith('"') or word.endswith('"'):
                 in_quotes = not in_quotes
                 continue
-            
-            # Skip words inside quotes
+
             if in_quotes:
                 continue
+            
+            if word in emphasis_modifiers:
+                emphasis_multiplier = emphasis_modifiers[word]
+                continue
 
-            # Check for negation
             if word in negation_words:
                 negation_active = not negation_active
                 continue
+
             word = stemmer.stem(word)
-            if word not in self.sentiment: continue
-            # Check positive lexicon
-            if self.sentiment[word] == "pos":
-                # Flip sentiment if negation is active
-                sentiment_score += (-1 if negation_active else 1)
-            
-            # Check negative lexicon
+            if word not in stemmed_dict: continue
+            if stemmed_dict[word] == "pos":
+                sentiment_score += emphasis_multiplier * (-1 if negation_active else 1)
+
             else:
-                # Flip sentiment if negation is active
-                sentiment_score += (1 if negation_active else -1)
-        
-        # Determine final sentiment
+                sentiment_score += emphasis_multiplier * (1 if negation_active else -1)
+
+            emphasis_multiplier = 1.0
+
         if sentiment_score > 0:
             return 1
         elif sentiment_score < 0:
@@ -342,26 +501,20 @@ class Chatbot:
         ########################################################################
         # TODO: Compute cosine similarity between the two vectors.             #
         ########################################################################
-        if np.all(u == 0) or np.all(v == 0):
-            return 0
-    
-        # Compute cosine similarity
-        # Use masked arrays to only compare rated items
-        mask = (u != 0) & (v != 0)
-        
-        # If no common ratings, return 0
-        if not np.any(mask):
-            return 0
-        
-        # Compute similarity only on common ratings
-        u_masked = u[mask]
-        v_masked = v[mask]
-        
-        # Compute cosine similarity
-        numerator = np.dot(u_masked, v_masked)
-        denominator = np.linalg.norm(u_masked) * np.linalg.norm(v_masked)
-        
-        return numerator / denominator if denominator != 0 else 0
+
+
+        u = np.ravel(u)
+        v = np.ravel(v)
+
+        dot_product = np.dot(u, v)
+
+        norm_u = np.linalg.norm(u)
+        norm_v = np.linalg.norm(v)
+
+        if norm_u == 0 or norm_v == 0:
+            return 0.0 
+
+        return dot_product / (norm_u * norm_v)
         ########################################################################
         #                          END OF YOUR CODE                            #
         ########################################################################
@@ -403,22 +556,23 @@ class Chatbot:
         # scores.                                                              #
         ########################################################################
 
-        # Populate this list with k movie indices to recommend to the user.
-        # similarity_matrix = self.similarity(ratings_matrix, ratings_matrix)
 
-        # # Compute predicted scores for each movie as the weighted sum of similar movies
-        # # user_ratings is a 1D array, we perform a dot product with similarity_matrix
-        # predicted_scores = similarity_matrix @ user_ratings.reshape(-1, 1)
-        # predicted_scores = predicted_scores.flatten()  # Convert back to 1D array
+        num_movies, _ = ratings_matrix.shape
 
-        # # Exclude movies the user has already rated (nonzero values in user_ratings)
-        # predicted_scores[user_ratings != 0] = -np.inf  # Set rated movies to -inf so they won't be recommended
+        norm_matrix = np.linalg.norm(ratings_matrix, axis=1, keepdims=True) 
+        norm_matrix[norm_matrix == 0] = 1 
+        similarity_matrix = (ratings_matrix @ ratings_matrix.T) / (norm_matrix @ norm_matrix.T)
 
-        # # Get the top-k movie indices with the highest predicted scores
-        # recommended_indices = np.argsort(predicted_scores)[-k:][::-1]  # Sort descending and get top k
+        np.fill_diagonal(similarity_matrix, 0)
 
-        # return recommended_indices.tolist()
-        return []
+        unrated_indices = np.where(user_ratings == 0)[0]
+
+        predicted_scores = similarity_matrix[unrated_indices] @ user_ratings
+
+        top_k_indices = unrated_indices[np.argsort(predicted_scores)[::-1][:k]]
+
+        return list(top_k_indices)
+
 
         ########################################################################
         #                        END OF YOUR CODE                              #
@@ -515,11 +669,10 @@ class Chatbot:
         """
 
         return """
-        Your task is to implement the chatbot as detailed in the PA7
-        instructions.
-        Remember: in the GUS mode, movie names will come in quotation marks
-        and expressions of sentiment will be simple!
-        TODO: Write here the description for your own chatbot!
+        This is a chatbot which gives movie recommendations. The user provides five movies 
+        and their thoughts on said movies, and the chatbot will recommend them up to five
+        movies which it thinks the user might like using sentiment analysis and a database of
+        user reviews.
         """
 
 
